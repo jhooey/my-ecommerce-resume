fe4b99f6f55503e3e521675f5ae84444fef94e37
Jacob Hooey <jhooey@gmail.com>
2014-03-09 21:33:09 -0400

    Converted the project to a rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyShopifyResume::Application.config.secret_key_base = '034b47704e2d9e0d5bc64df96319c11f38c457ef1e5e3fbb8400f55821031feb37a40602ad182a2b2281c45f9c9b27c1e2c14e8c0813e294ecd6102159fecd2c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyShopifyResume::Application.config.session_store :cookie_store, key: '_my-shopify-resume_session'
