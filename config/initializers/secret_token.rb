14b85bbdc50faa035fc833ea1156b8177b8457cb
Jacob Hooey <jhooey@gmail.com>
2014-03-11 13:13:22 -0400

    initialized rails server

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-MyShopifyResume::Application.config.secret_key_base = '034b47704e2d9e0d5bc64df96319c11f38c457ef1e5e3fbb8400f55821031feb37a40602ad182a2b2281c45f9c9b27c1e2c14e8c0813e294ecd6102159fecd2c'
+MyShopifyResume::Application.config.secret_key_base = '4199d54b598c8afdb987772c524aa52f38eb226bef8c5b29b0dbf1b60ca27d109f0d0b3f7e60d1ba464a5e0cf0f0aeb20c0d9b716eb9109831f09964ee3ac040'
