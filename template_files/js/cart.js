cc898c4890a650db9f775befdc2247aa1497d413
Jacob Hooey <jhooey@gmail.com>
2014-03-11 10:56:11 -0400

    initialize cart variables

--- template_files/js/cart.js
+++ template_files/js/cart.js
@@ -10,6 +10,31 @@
 			// initializes properties and methods
 			this.cartName = "resume-cart";
 			this.storage = sessionStorage;
+			
+			this.$formAddToCart = this.$element.find( "form.add-to-cart" );
+			this.$formCart = this.$element.find( "#shopping-cart" );
+			this.$checkoutCart = this.$element.find( "#checkout-cart" );
+			this.$checkoutOrderForm = this.$element.find( "#checkout-order-form" );
+			this.$shoppingCartActions = this.$element.find( "#shopping-cart-actions" );
+			this.$updateCartBtn = this.$shoppingCartActions.find( "#update-cart" );
+			this.$emptyCartBtn = this.$shoppingCartActions.find( "#empty-cart" );
+			this.$userDetails = this.$element.find( "#user-details-content" );
+			
+			// object containing patterns for form validation
+			this.requiredFields = {
+				expression: {
+					value: /^([\w-\.]+)@((?:[\w]+\.)+)([a-z]){2,4}$/
+				},
+
+				str: {
+					value: ""
+				}
+
+			};
+			
+			// public methods invocation
+			
+			
 		}
 	};
 
