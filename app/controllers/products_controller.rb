738fc78ef5da460b9d5a3d5dcb10f5a97a927f8d
Jacob Hooey <jhooey@gmail.com>
2014-03-12 17:50:02 -0400

    Created list of products and show individual product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,24 @@
 class ProductsController < ApplicationController
   def new
   end
+
+  def index
+    @products = Product.all
+  end
    
   def create
-    render text: params[:product].inspect
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to @product
   end
+  
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+   params.require(:product).permit(:title, :text)
+  end
+
+
 end
