dccf810e5632a7418c1377fe2b1768542e87f40f
Jacob Hooey <jhooey@gmail.com>
2014-03-13 22:40:51 -0400

    converted back to postgresql because heroku does not support sqlite

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-MyShopifyResume::Application.config.secret_key_base = '658a0f91e9d48d25603030791818859ef0ba5f6a4d18fbf49eef3106f437808abb3bf8ed6544dcf6ae2a3e80dd42c8410e268d249448ac6646196d2de4356bfe'
+MyShopifyResume::Application.config.secret_key_base = '30b107b4c01f014de0b58dbfa03a48c072800273d9421ab09802e36ba6592e56ba459dc0dd5aa592186465339755e9b59dd6162d4f083e4404cde36399ef12cc'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140314022335) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: true do |t|
     t.string   "title"
     t.text     "short"
