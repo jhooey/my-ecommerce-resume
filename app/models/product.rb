43cfc6ea2f553b53f5eb0cf6bf25a0b71a467d6a
Jacob Hooey <jhooey@gmail.com>
2014-03-17 11:24:41 -0400

    sort items by date created

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,4 @@
 class Product < ActiveRecord::Base
   scope :is_featured, lambda {|featured_boolean| {:conditions => {:featured => featured_boolean}}}
   serialize :images
-  
-  default_scope order('created_at DESC')
 end
