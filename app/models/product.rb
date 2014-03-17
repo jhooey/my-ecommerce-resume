11d3c9653cbfde8403d28acebad9ee597183d1b3
Jacob Hooey <jhooey@gmail.com>
2014-03-17 10:35:07 -0400

    added default sort order

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
   scope :is_featured, lambda {|featured_boolean| {:conditions => {:featured => featured_boolean}}}
   serialize :images
+  
+  default_scope order('created_at DESC')
 end
